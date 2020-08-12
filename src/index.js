023c8d44ffd4d7dd8686176bb58493dd2a2e0973
ADILELBOURKI <adilbcsm@gmail.com>
2020-08-12 18:11:32 +0100

    wrapping cards into an array of cards

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ import "./index.css";
 import "tachyons";
 import "./robots";
 import Card from "./Card";
+
 import * as serviceWorker from "./serviceWorker";
 import { robots } from "./robots";
+import CardList from "./CardList";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div>
+    <CardList robots={robots} />
   </React.StrictMode>,
   document.getElementById("root")
 );
