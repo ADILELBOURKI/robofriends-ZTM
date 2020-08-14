bcede45c4822ab990df87a844ce7540576ed1e30
ADILELBOURKI <adilbcsm@gmail.com>
2020-08-14 10:50:23 +0100

    componentdidmount  added

--- src/App.js
+++ src/App.js
@@ -7,19 +7,36 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: "",
     };
   }
-  onSearchChange(event) {
-    console.log(event.target.value);
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
+  onSearchChange = (event) => {
+    this.setState({
+      searchField: event.target.value,
+    });
+  };
+
   render() {
+    const filteredrobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots} />;
+        <CardList robots={filteredrobots} />;
       </div>
     );
   }
